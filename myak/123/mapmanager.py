82781ce7c85fc70fd8eb0e60e7e65c4665256d29
Fikus202 <vitall34981@gmail.com>
2025-11-09 16:47:52 +0200

    Add resyrs

---
+++ myak/123/mapmanager.py
@@ -0,0 +1,8 @@
+class mapmanager():
+
+    def __init__(self):
+            self.model = "block.egg"
+            self.texture = "brick.png"
+            self.color = (0.2,0.2,0.35,1)
+    def addBlock(self,position):
+          pass
